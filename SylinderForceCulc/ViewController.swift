97d13a36170c4fce858047276f885c2b390910b6
H-NGT <nov.13.v3.0@gmail.com>
2022-07-18 08:44:52 +0900

    Initial Commit

---
+++ SylinderForceCulc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SylinderForceCulc
+//
+//  Created by 永田浩之 on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
